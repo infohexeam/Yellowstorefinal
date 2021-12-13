ad0db914e017c79254b3bb684947be7fb940d5e8
Jinsi <jizjincy70@gmail.com>
2021-12-13 17:30:24 +0530

    route change

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/admin-dashboard', function () {
     return view('admin.dashboard');
 });
 
-// Auth::routes();
+Auth::routes();
 Route::get('/qr', 'store\CouponController@makeQr');
 Route::get('/geotest', 'store\CouponController@geotest');
 
