27e6ffa5a348f396909e2e0057df7a33ad305059
binu-p-benny <binupb39@outlook.com>
2022-01-10 17:46:37 +0530

    style updates

--- resources/views/admin/masters/attribute_group/edit.blade.php
+++ resources/views/admin/masters/attribute_group/edit.blade.php
@@ -31,7 +31,7 @@
                         @csrf
                
               <div class="form-body">
-                <div class="row" style="min-height: 72em;"
+                <div class="row" style="min-height: 72vh;"></div>
                     <div class="col-md-12">
                         <div class="form-group">
                             <label class="form-label">Atribute Group Name</label>
