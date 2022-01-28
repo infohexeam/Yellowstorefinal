e02e066c5507762242b5a308b51308263590bf9f
binu-p-benny <binupb39@outlook.com>
2022-01-28 13:39:30 +0530

    .

--- app/Models/admin/Mst_categories.php
+++ app/Models/admin/Mst_categories.php
@@ -7,23 +7,28 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Mst_categories extends Model
 {
-    use SoftDeletes;
+	use SoftDeletes;
 
-  	protected $table ="mst_store_categories";
+	protected $table = "mst_store_categories";
 	protected $primaryKey = "category_id";
 
-     protected $fillable = [
-    					'category_id ','parent_id','business_type_id','category_name','category_name_slug','category_icon','category_description','category_status',
-	  
-	  					  ];
+	protected $fillable = [
+		'category_id ', 'parent_id', 'business_type_id', 'category_name', 'category_name_slug', 'category_icon', 'category_description', 'category_status',
 
-	
-	   public function categories()
-	 {
-	 	 return $this->belongsTo('App\Models\admin\Mst_categories','parent_id','category_id');
-	 }
-	   public function business_type()
-	 {
-	 	 return $this->belongsTo('App\Models\admin\Mst_business_types','business_type_id','business_type_id');
-	 }
+	];
+
+
+	public function categories()
+	{
+		return $this->belongsTo('App\Models\admin\Mst_categories', 'parent_id', 'category_id');
+	}
+	public function business_type()
+	{
+		return $this->belongsTo('App\Models\admin\Mst_business_types', 'business_type_id', 'business_type_id');
+	}
+
+	public function business_types()
+	{
+		return $this->hasMany('App\Models\admin\Trn_CategoryBusinessType', 'category_id', 'category_id');
+	}
 }
