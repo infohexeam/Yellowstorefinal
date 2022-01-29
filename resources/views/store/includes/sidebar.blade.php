5eae2b6f961020cdcea73cb6b4e6037ffb620dc5
binu-p-benny <binupb39@outlook.com>
2022-01-29 16:23:19 +0530

    .

--- resources/views/store/includes/sidebar.blade.php
+++ resources/views/store/includes/sidebar.blade.php
@@ -135,6 +135,7 @@
 
               <li><a class="slide-item" href="{{route('store.payment_reports')}}">{{ __('Payment Reports') }}</a></li>
               <li><a class="slide-item" href="{{route('store.delivery_reports')}}">{{ __('Delivery Reports') }}</a></li>
+              <li><a class="slide-item" href="{{url('store/incoming-payments')}}">{{ __('Incoming Payments Reports') }}</a></li>
 
             </ul>
         </li>
