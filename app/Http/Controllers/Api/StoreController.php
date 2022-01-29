613e12c6d4c69fc93a332f2834afa736ed793309
binu-p-benny <binupb39@outlook.com>
2022-01-29 15:19:17 +0530

    .

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -2055,9 +2055,9 @@ class StoreController extends Controller
                     ->join('trn_store_orders', 'trn_store_orders.order_id', '=', 'trn__order_payment_transactions.order_id')
                     ->join('trn_store_customers', 'trn_store_customers.customer_id', '=', 'trn_store_orders.customer_id')
                     ->leftjoin('mst_delivery_boys', 'mst_delivery_boys.delivery_boy_id', '=', 'trn_store_orders.delivery_boy_id')
-                     ->where('trn__order_split_payments.paymentRole', '=', 1)
-                    ->where('trn_store_orders.store_id', '=', $store_id);
-
+                    ->where('trn__order_split_payments.paymentRole', '=', 1)
+                    ->where('trn_store_orders.store_id', '=', $store_id)->get();
+                dd($paymentReport);
 
 
 
