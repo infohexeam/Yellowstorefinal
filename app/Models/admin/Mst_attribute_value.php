27f40dd40357a35f40225eeee15ec5954c9a7f01
binu-p-benny <binupb39@outlook.com>
2022-02-07 15:58:13 +0530

    .

--- app/Models/admin/Mst_attribute_value.php
+++ app/Models/admin/Mst_attribute_value.php
@@ -3,18 +3,18 @@
 namespace App\Models\admin;
 
 use Illuminate\Database\Eloquent\Model;
-//use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Mst_attribute_value extends Model
 {
-        // use softDeletes;
- protected $primaryKey = "attr_value_id";
+   use softDeletes;
+   protected $primaryKey = "attr_value_id";
 
-     protected $fillable = [
-    					'attr_value_id','group_value','attribute_group_id',				
-    						  ];
-public function attr_group()
+   protected $fillable = [
+      'attr_value_id', 'group_value', 'attribute_group_id',
+   ];
+   public function attr_group()
    {
-   	return $this->belongsTo('App\Models\admin\Mst_attribute_group','attribute_group_id','attr_group_id');
+      return $this->belongsTo('App\Models\admin\Mst_attribute_group', 'attribute_group_id', 'attr_group_id');
    }
 }
