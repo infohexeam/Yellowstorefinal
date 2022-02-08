6509aa989a579fec378fa1c9e173e9a2c17fa79c
binu-p-benny <binupb39@outlook.com>
2022-02-08 16:36:27 +0530

    .

--- app/Models/admin/Mst_business_types.php
+++ app/Models/admin/Mst_business_types.php
@@ -9,14 +9,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Mst_business_types extends Model
 {
-    use SoftDeletes;
+	use SoftDeletes;
 
-  	protected $table ="mst_store_business_types";
+	protected $table = "mst_store_business_types";
 	protected $primaryKey = "business_type_id";
 
-     protected $fillable = [
-    					'business_type_id','business_type_name','business_type_name_slug','business_type_icon','business_type_status',	 ]; 					  ];
-
-	
-	 
+	protected $fillable = [
+		'business_type_id', 'business_type_name', 'business_type_name_slug', 'business_type_icon', 'business_type_status'
+	];
 }
