0f19336b715f3cdf5d0f39bb878ea7ddc9e1dd27
binu-p-benny <binupb39@outlook.com>
2022-02-15 13:15:53 +0530

    .

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -1763,11 +1763,11 @@ class StoreController extends Controller
                 $inventoryDatas = $inventoryDatasss->unique('product_varient_id');
                 $dataReViStoreSS =   $inventoryDatas->values()->all();
 
-                foreach ($dataReViStoreSS as $dva) {
-                    $sDetail = Mst_StockDetail::where('product_varient_id', $dva->product_varient_id)->orderBy('stock_detail_id', 'DESC')->first();
-                    $dva->created_at = $sDetail->created_at;
-                    $dva->updated_time = $sDetail->created_at;
-                }
+                // foreach ($dataReViStoreSS as $dva) {
+                //     $sDetail = Mst_StockDetail::where('product_varient_id', $dva->product_varient_id)->orderBy('stock_detail_id', 'DESC')->first();
+                //     $dva->created_at = $sDetail->created_at;
+                //     $dva->updated_time = $sDetail->created_at;
+                // }
 
 
                 $data['inventoryData'] = $dataReViStoreSS;
