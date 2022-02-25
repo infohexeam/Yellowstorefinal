6dfd137237d30e4110f65306c3a05278797ee7e2
binu-p-benny <binupb39@outlook.com>
2022-02-25 16:40:24 +0530

    .

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -429,7 +429,7 @@ class OrderController extends Controller
                             else
                                 $value->product_name = @$baseProductDetail->product_name;
 
-                            $taxFullData = Mst_Tax::withTrashed()->find(@$baseProductDetail->tax_id);
+                            $taxFullData = Mst_Tax::find(@$baseProductDetail->tax_id);
 
                             // $gstAmount = $value['productDetail']->product_varient_offer_price * $baseProductDetail->tax_value / (100 + $baseProductDetail->tax_value);
                             // $orgCost = $value['productDetail']->product_varient_offer_price * 100 / (100 + $baseProductDetail->tax_value);
@@ -448,11 +448,11 @@ class OrderController extends Controller
                             $splitdata = [];
 
                             if (isset($taxFullData)) {
-                                $splitdata = \DB::table('trn__tax_split_ups')->where('tax_id', @$baseProductDetail->tax_id)->withTrashed()->get();
+                                $splitdata = \DB::table('trn__tax_split_ups')->where('tax_id', @$baseProductDetail->tax_id)->get();
 
                                 foreach ($splitdata as $sd) {
                                     if (@$taxFullData->tax_value == 0 || !isset($taxFullData->tax_value))
-                                        @$taxFullData->tax_value = 1;
+                                        $taxFullData->tax_value = 1;
 
                                     $stax = ($sd->split_tax_value * $tTax) / @$taxFullData->tax_value;
                                     $sd->tax_split_value = number_format((float)$stax, 2, '.', '');
