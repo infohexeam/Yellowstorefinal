50f6d8703805bfc328456056f62b9e54b0a656ee
binu-p-benny <binupb39@outlook.com>
2022-03-01 09:37:23 +0530

    .

--- app/Models/admin/Mst_Tax.php
+++ app/Models/admin/Mst_Tax.php
@@ -4,13 +4,14 @@ namespace App\Models\admin;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Mst_Tax extends Model
 {
    use SoftDeletes;
-    protected $primaryKey = "tax_id";
-    protected $table = "mst__taxes";
+   protected $primaryKey = "tax_id";
+   protected $table = "mst__taxes";
 
    protected $fillable = [
-                           'tax_value','tax_name'
-               ];
+      'tax_value', 'tax_name', 'is_removed'
+   ];
 }
