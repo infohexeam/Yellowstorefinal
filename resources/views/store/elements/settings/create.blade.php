e3941b2992067694096ea71a5bfdf443936d87dc
binu-p-benny <binupb39@outlook.com>
2022-03-11 08:36:00 +0530

    .

--- resources/views/store/elements/settings/create.blade.php
+++ resources/views/store/elements/settings/create.blade.php
@@ -219,7 +219,7 @@
 </div>
 
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 
 <script>
 function makeValue(val)
