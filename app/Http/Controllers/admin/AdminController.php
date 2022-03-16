046c0d3a7b66c7d0b9909222f19a5cad5e4f0580
binu-p-benny <binupb39@outlook.com>
2022-03-16 15:49:20 +0530

    .

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -792,6 +792,7 @@ class AdminController extends Controller
                 ],
                 [
                     'images.*.dimensions' => 'Banner image dimensions invalid',
+
                 ]
             );
             if ($img_validate->fails()) {
