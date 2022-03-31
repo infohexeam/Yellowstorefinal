29a0d22318d142004fadf45d4b286bee9e3b4735
binu-p-benny <binupb39@outlook.com>
2022-03-31 10:28:24 +0530

    assets

---
+++ public/assets/assets/plugins/datatable/datatable.js
@@ -0,0 +1,12 @@
+$(function(e) {
+	 $('#example').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+             'excel','pdf', 
+        ]
+    } );
+
+} );
+$(function(e) {
+	$('#example5').DataTable();
+} );
