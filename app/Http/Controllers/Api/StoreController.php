97d4050ebdd703b002b5dcff49c386e9f0cb9501
binu-p-benny <binupb39@outlook.com>
2022-04-04 18:12:16 +0530

    .

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -1893,50 +1893,47 @@ class StoreController extends Controller
             if (isset($request->store_id) && Mst_store::find($request->store_id)) {
                 $store_id = $request->store_id;
 
-                $inventoryData =  Mst_StockDetail::select(
-                    'mst__stock_details.*',
-                    'mst__stock_details.updated_at AS updated_time',
-
-                    'mst_store_products.product_name',
-                    'mst_store_products.product_code',
-                    'mst_store_products.product_cat_id',
-                    'mst_store_products.product_base_image',
-                    'mst_store_products.product_status',
-                    'mst_store_products.product_brand',
-                    'mst_store_products.min_stock',
-
-                    'mst_store_products.tax_id',
-                    'mst_store_product_varients.variant_name',
-                    'mst_store_product_varients.product_varient_price',
-                    'mst_store_product_varients.product_varient_offer_price',
-                    'mst_store_product_varients.product_varient_base_image',
-                    'mst_store_product_varients.stock_count',
-                    'mst_store_categories.category_id',
-                    'mst_store_categories.category_name',
-                    'mst_store_agencies.agency_name',
-                    'mst__sub_categories.sub_category_name',
-
-                )->join('mst_store_product_varients', 'mst_store_product_varients.product_varient_id', '=', 'mst__stock_details.product_varient_id')
-                    ->join('mst_store_products', 'mst_store_products.product_id', '=', 'mst__stock_details.product_id')
+                $inventoryData =  Mst_store_product_varient::join('mst_store_products', 'mst_store_products.product_id', '=', 'mst_store_product_varients.product_id')
                     ->join('mst_store_categories', 'mst_store_categories.category_id', '=', 'mst_store_products.product_cat_id')
+                    ->leftjoin('mst_stock_details', 'mst_stock_details.product_varient_id', '=', 'mst_store_product_varients.product_varient_id')
                     ->leftjoin('mst_store_agencies', 'mst_store_agencies.agency_id', '=', 'mst_store_products.vendor_id')
-                    ->leftjoin('mst__sub_categories', 'mst__sub_categories.sub_category_id', '=', 'mst_store_products.sub_category_id')
-
-                    // $inventoryData =  Mst_store_product_varient::join('mst_store_products', 'mst_store_products.product_id', '=', 'mst_store_product_varients.product_id')
-                    //     ->join('mst_store_categories', 'mst_store_categories.category_id', '=', 'mst_store_products.product_cat_id')
-                    //     ->leftjoin('mst__stock_details', 'mst__stock_details.product_varient_id', '=', 'mst_store_product_varients.product_varient_id')
-                    //     ->leftjoin('mst_store_agencies', 'mst_store_agencies.agency_id', '=', 'mst_store_products.vendor_id')
-                    //     ->leftjoin('mst__sub_categories', 'mst__sub_categories.sub_category_id', '=', 'mst_store_products.sub_category_id')
+                    ->leftjoin('mst_sub_categories', 'mst_sub_categories.sub_category_id', '=', 'mst_store_products.sub_category_id')
 
                     ->where('mst_store_products.store_id', $store_id)
                     ->where('mst_store_product_varients.stock_count', '<=', 0)
                     ->where('mst_store_products.product_type', 1)
                     // ->orderBy('mst_store_products.product_name','ASC')
-                    ->where('mst_store_products.is_removed', 0)
-                    ->where('mst_store_product_varients.is_removed', 0);
+                    ->orderBy('mst__stock_details.created_at', 'DESC')
+
+                    ->select(
+                        'mst_store_products.product_id',
+                        'mst_store_products.product_name',
+                        'mst_store_products.product_code',
+                        'mst_store_products.product_cat_id',
+                        'mst_store_products.product_base_image',
+                        'mst_store_products.product_status',
+                        'mst_store_products.product_brand',
+                        'mst_store_products.min_stock',
+
+                        'mst_store_products.tax_id',
+                        'mst_store_product_varients.product_varient_id',
+                        'mst_store_product_varients.variant_name',
+                        'mst_store_product_varients.product_varient_price',
+                        'mst_store_product_varients.product_varient_offer_price',
+                        'mst_store_product_varients.product_varient_base_image',
+                        'mst_store_product_varients.stock_count',
+                        'mst__stock_details.created_at AS updated_time',
+                        //'mst__stock_details.created_at',
+                        'mst_store_categories.category_id',
+                        'mst_store_categories.category_name',
+                        'mst__stock_details.stock',
+                        'mst__stock_details.prev_stock',
+                        'mst_store_agencies.agency_name',
+                        'mst__sub_categories.sub_category_name',
+
+                    );
 
 
-                    
 
                 $datefrom = $request->date_from;
                 $dateto = $request->date_to;
