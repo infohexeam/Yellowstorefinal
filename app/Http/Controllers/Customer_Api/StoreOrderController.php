a54d6623982f8b96d9bfbbe33ca37187dd9fb644
Jinsi <jizjincy70@gmail.com>
2022-04-22 18:25:15 +0530

    .

--- app/Http/Controllers/Customer_Api/StoreOrderController.php
+++ app/Http/Controllers/Customer_Api/StoreOrderController.php
@@ -1136,7 +1136,7 @@ class StoreOrderController extends Controller
                     'refundId'
                 )->where('customer_id', $request->customer_id)->orderBy('order_id', 'DESC')->get()) {
                     foreach ($data['orderHistory'] as $order) {
-                        $storeData = Mst_store::find($order->store_id);
+                        $storeData = Mst_store::find($order->store_id)->withTrashed();
                         $order->store_name = $storeData->store_name;
                         if (isset($order->customer_id)) {
                             $customerData = Trn_store_customer::find($order->customer_id);
