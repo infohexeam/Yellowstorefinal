fe8101f9b8efdfb91aa9c8a673ca0387045e2b4a
Jinsi <jizjincy70@gmail.com>
2022-04-22 20:54:13 +0530

    .

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -1060,7 +1060,7 @@ class ProductController extends Controller
             // 'mst__sub_categories.sub_category_name'
           )
             // ->join('trn_store_customers', 'trn_store_customers.customer_id', '=', 'trn__recently_visited_products.customer_id')
-            ->join('mst_stores', 'mst_stores.store_id', '=', 'trn__recently_visited_products.store_id')
+            ->join('mst_stores', 'mst_stores.store_id', '=', 'trn__recently_visited_products.store_id');
             // ->join('mst_store_products', 'mst_store_products.product_id', '=', 'trn__recently_visited_products.product_id')
             // ->join('mst_store_product_varients', 'mst_store_product_varients.product_varient_id', '=', 'trn__recently_visited_products.product_varient_id')
             // ->join('mst_store_agencies', 'mst_store_agencies.agency_id', '=', 'mst_store_products.vendor_id')
