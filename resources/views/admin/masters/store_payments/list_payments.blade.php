e351731e64f4111b9a67964eed761675ac20bfff
Jinsi <jizjincy70@gmail.com>
2022-04-26 00:58:05 +0530

    .

--- resources/views/admin/masters/store_payments/list_payments.blade.php
+++ resources/views/admin/masters/store_payments/list_payments.blade.php
@@ -102,7 +102,9 @@
                            <center>
                            <button type="submit" class="btn btn-raised btn-primary">
                            <i class="fa fa-check-square-o"></i> Filter</button>
-                           <button type="reset"  id="reset" class="btn btn-raised btn-success">Reset</button>
+                           
+                           
+                           <a href="{{route('admin.list_payment_settlments')}}"  class="btn btn-info">Cancel</a>
                            </center>
                         </div>
                   </div>
