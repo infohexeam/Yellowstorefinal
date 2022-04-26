8625b75a6a79ebb785891688b51078fa0b11eec1
Jinsi <jizjincy70@gmail.com>
2022-04-26 13:27:05 +0530

    .

--- resources/views/store/elements/reports/product_report.blade.php
+++ resources/views/store/elements/reports/product_report.blade.php
@@ -337,7 +337,7 @@ $(document).ready(function() {
                 title: 'Product report',
                 footer: true,
                 exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12]
                  },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
