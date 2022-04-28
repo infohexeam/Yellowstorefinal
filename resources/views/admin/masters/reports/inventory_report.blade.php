11622b4cbbb4b644118e0832bbbaf359cab34c10
Jinsi <jizjincy70@gmail.com>
2022-04-28 16:05:44 +0530

    .

--- resources/views/admin/masters/reports/inventory_report.blade.php
+++ resources/views/admin/masters/reports/inventory_report.blade.php
@@ -255,7 +255,7 @@
                 title: 'Inventory report',
                 footer: true,
                 exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
                  },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
@@ -265,7 +265,7 @@
                 title: 'Inventory report',
                 footer: true,
                 exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
                  }
             }
          ]
