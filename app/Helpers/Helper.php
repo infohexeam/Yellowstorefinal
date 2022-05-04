e187a768509a5dcf348f4fc44958d6b00dc04397
Jinsi <jizjincy70@gmail.com>
2022-05-04 13:05:09 +0530

    notification

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -540,7 +540,8 @@ class Helper
         $api_key = 'AAAA09gixf4:APA91bFiBdhtMnj2UBtqSQ9YlZ_uxvdOOOzE-otA9Ja2w0cFUpX230Xv0Yi87owPBlFDp1H02FWpv4m8azPsuMmeAmz0msoeF-1Cxx0iVpDSOjYBTCWxzUYT8tKTuUvLb08MDsRXHbgM';
         $fields = array(
             'to' => $device_id,
-            'notification' => array('title' => $title, 'body' => $body, 'sound' => 'default', 'click_action' => 'order'),
+            'notification' => array('title' => $title, 'body' => $body, 'sound' => 'default'),
+            'data' => array('type' => 'order')
         );
         $headers = array(
             'Content-Type:application/json',
