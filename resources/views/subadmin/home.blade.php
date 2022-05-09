043e2f152a9f031814a785cc7295efce56091548
Jinsi <jizjincy70@gmail.com>
2022-05-09 23:36:14 +0530

    ..

--- resources/views/subadmin/home.blade.php
+++ resources/views/subadmin/home.blade.php
@@ -21,7 +21,7 @@ use App\Models\admin\Trn_sub_admin_payment_settlment;
 
    $storesSubadmins = Mst_store::where('subadmin_id', auth()->user()->id)->pluck('store_id');
 			
-	$delivery_boys = \DB::table('mst_delivery_boys')
+	$boys_count = \DB::table('mst_delivery_boys')
 				->join('mst_store_link_delivery_boys', 'mst_store_link_delivery_boys.delivery_boy_id', '=', 'mst_delivery_boys.delivery_boy_id')
 				->whereIn('mst_store_link_delivery_boys.store_id', $storesSubadmins)
 				->orderBy('mst_delivery_boys.delivery_boy_id', 'DESC')
