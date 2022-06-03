02f78a7fc52e944ea31e6145c42e05f910090141
Jinsi <jizjincy70@gmail.com>
2022-06-03 23:24:16 +0530

    banner-export

--- resources/views/admin/masters/banners/customer_app_banners/list.blade.php
+++ resources/views/admin/masters/banners/customer_app_banners/list.blade.php
@@ -35,7 +35,7 @@
                                       </ul>
                                    </div>
                                    @endif
-                            <table id="example" class="table table-striped table-bdataed text-nowrap w-100">
+                            <table id="exampletable" class="table table-striped table-bdataed text-nowrap w-100">
                                 <thead>
                                     <tr>
                                         <th class="wd-15p">SL.No</th>
@@ -368,7 +368,43 @@ function findTown(city_id){
         });
 }
 </script>
+<script>
 
+    $(function(e) {
+         $('#exampletable').DataTable( {
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'pdf',
+                    title: 'Customer app Banners',
+                    // orientation:'landscape',
+                    footer: true,
+                    exportOptions: {
+                         columns: [0,2,3],
+                         alignment: 'right',
+                     },
+                      customize: function(doc) {
+                          doc.content[1].margin = [ 100, 0, 100, 0 ]; //left, top, right, bottom
+                       doc.content.forEach(function(item) {
+                        if (item.table) {
+                            item.table.widths = [40, '*','*']
+                         }
+                       })
+                     }
+                },
+                {
+                    extend: 'excel',
+                    title: 'Customer app Banners',
+                    footer: true,
+                    exportOptions: {
+                         columns: [0,2,3]
+                     }
+                }
+             ]
+        } );
+    
+    } );
+                </script>
 
 
 
