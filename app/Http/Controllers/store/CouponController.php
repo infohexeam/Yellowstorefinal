1b132d431f6e25a0b0317f024cafbc68c638ece7
Jinsi <jizjincy70@gmail.com>
2022-06-09 14:17:50 +0530

    .

--- app/Http/Controllers/store/CouponController.php
+++ app/Http/Controllers/store/CouponController.php
@@ -1040,7 +1040,7 @@ class CouponController extends Controller
 
         
         if (isset($request->customer_mobile_number)) {
-          $fetchCustomerData = Trn_store_customer::where('trn_store_customers.customer_mobile_number', 'LIKE', '%' . $request->customer_mobile_number . '%')->first();
+          $fetchCustomerData = Trn_store_customer::where('trn_store_customers.customer_mobile_number', 'LIKE','%'.$request->customer_mobile_number.'%')->first();
           if($fetchCustomerData != NULL)
           {
               $data = $data->where('trn_store_customers.customer_id', '=', $fetchCustomerData->customer_id);
