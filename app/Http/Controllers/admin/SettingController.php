3a01666598114db0075ab65a78781531c7e2a56f
Jinsi <jizjincy70@gmail.com>
2022-06-20 09:57:55 +0530

    .

--- app/Http/Controllers/admin/SettingController.php
+++ app/Http/Controllers/admin/SettingController.php
@@ -3388,8 +3388,8 @@ class SettingController extends Controller
 			
 
 			if ($store_id == 0 && isset($subadmin_id)) {
-				
-				$store_data = DB::table('trn_store_orders')->select("store_id")->where('subadmin_id', '=',$subadmin_id)->where('subadmin_id', '!=',NULL)->get();
+
+				$store_data = DB::table('mst_stores')->select("store_id")->where('subadmin_id', '=',$subadmin_id)->where('subadmin_id', '!=',NULL)->get();
 				$store_array[] = 0;
 				foreach ($store_data as $val) {
 					$store_array[] = $val->store_id;
@@ -3398,7 +3398,7 @@ class SettingController extends Controller
 				//$query = $query->where('store_id', $store_id);
 			} elseif ($store_id == 0 && !isset($subadmin_id)) {
 
-				$store_data = \DB::table('trn_store_orders')->select("store_id")->get();
+				$store_data = \DB::table('mst_stores')->select("store_id")->get();
 				foreach ($store_data as $val) {
 					$store_array[] = $val->store_id;
 				}
