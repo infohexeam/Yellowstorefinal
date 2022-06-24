264b8e607deef6cccae1070bf9221b657bf5a430
Jinsi <jizjincy70@gmail.com>
2022-06-24 15:47:20 +0530

    weekly visit app

--- app/Http/Controllers/Api/StoreSettingsController.php
+++ app/Http/Controllers/Api/StoreSettingsController.php
@@ -897,7 +897,7 @@ class StoreSettingsController extends Controller
                 $recentvisitCountToday = Trn_RecentlyVisitedStore::whereDate('created_at', Carbon::today())
                     ->where('store_id', $request->store_id)->count();
 
-                $recentvisitCountWeek = Trn_RecentlyVisitedStore::where('created_at', '>', Carbon::now()->subDays(2))
+                $recentvisitCountWeek = Trn_RecentlyVisitedStore::where('created_at', '>', Carbon::now()->subDays(6))
                     ->where('store_id', $request->store_id)->count();
 
                 $recentvisitCountMonth = Trn_RecentlyVisitedStore::where('created_at', '>', Carbon::now()->subDays(30))
