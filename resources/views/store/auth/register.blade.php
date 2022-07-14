aa3ea81bc523b326f00cad573e8f178829ce235b
Jinsi <jizjincy70@gmail.com>
2022-07-14 18:49:48 +0530

    sms-p4

--- resources/views/store/auth/register.blade.php
+++ resources/views/store/auth/register.blade.php
@@ -454,7 +454,6 @@
 <script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js"></script>
 <script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-auth.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 
       
       <!-- BACKGROUND-IMAGE CLOSED -->
