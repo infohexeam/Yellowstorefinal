24a17e842d1a9d985da5c422d0621c815b9d248b
Jinsi <jizjincy70@gmail.com>
2022-10-21 13:11:50 +0530

    eye button store

--- resources/views/store/elements/password/update_password.blade.php
+++ resources/views/store/elements/password/update_password.blade.php
@@ -3,6 +3,42 @@
 @extends('store.layouts.app')
 
 @section('content')
+
+<style>
+
+  .password-show {
+    position: relative;
+  }
+  .password-show input {
+    padding-right: 2.5rem;
+  }
+  .password-show__toggle {
+    position: absolute;
+    top: 15px;
+    right: 0;
+    bottom: 0;
+    width: 2.5rem;
+  }
+  .password-show_toggleshow-icon, .password-showtoggle_hide-icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #555;
+  }
+  .password-show_toggle_show-icon {
+    display: block;
+  }
+  .password-show.show .password-show_toggle_show-icon {
+    display: none;
+  }
+  .password-show_toggle_hide-icon {
+    display: none;
+  }
+  .password-show.show .password-show_toggle_hide-icon {
+    display: block;
+  }
+  </style>
  <div class="container">
 	<div class="row">
 	  <div class="col-md-12">
@@ -46,7 +82,13 @@
 			<div class="col-md-12">
 			<div class="form-group">
 				<label class="form-label">Old Password</label>
+        <div class="password-show">
 					  <input type="password" required class="form-control" name="old_password" id="old_password" value="" placeholder="Old Password">
+            <div class="password-show__toggle">
+              <i class="fa fa-eye password-show_toggle_show-icon"></i>
+              <i class="fa fa-eye-slash password-show_toggle_hide-icon"></i>
+            </div> 
+        </div>
                </div>
 				   </div>
 
@@ -54,15 +96,27 @@
 				<div class="col-md-12">
 			<div class="form-group">
 				<label class="form-label">New Password</label>
+        <div class="password-show">
 					  <input type="password" class="form-control" oninput="checkPasswordComplexity(this.value)" onkeyup="validatePassLength()" name="password" id="password" value="" placeholder="New Password">
+            <div class="password-show__toggle">
+              <i class="fa fa-eye password-show_toggle_show-icon"></i>
+              <i class="fa fa-eye-slash password-show_toggle_hide-icon"></i>
+            </div> 
                         <span id="showpassmessage"></span>
                         <span id="showpassmessage2"></span>
+        </div>
                </div>
 				   </div>
 				  <div class="col-md-12">
 				 <div class="form-group">
 						<label class="form-label">Password Confirmation</label>
+            <div class="password-show">
 					  <input onkeyup="validatePass()" id="confirm_password" type="password" class="form-control" name="password_confirmation" value="" placeholder="Confirm Password">
+            <div class="password-show__toggle">
+              <i class="fa fa-eye password-show_toggle_show-icon"></i>
+              <i class="fa fa-eye-slash password-show_toggle_hide-icon"></i>
+            </div> 
+            </div>
 				                        <span id="showmessage"></span>
 
                 </div>
@@ -93,6 +147,33 @@
             </div>
         </div>
     </div>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+  $(".password-show__toggle").on("click", function(e) {
+    console.log("click");
+    if (
+      !$(this)
+        .parent()
+        .hasClass("show")
+    ) {
+      $(this)
+        .parent()
+        .addClass("show");
+      $(this)
+        .prev()
+        .attr("type", "text");
+    } else {
+      $(this)
+        .parent()
+        .removeClass("show");
+      $(this)
+        .prev()
+        .attr("type", "password");
+    }
+  });
+});
+   </script>
 
 
     <script>
