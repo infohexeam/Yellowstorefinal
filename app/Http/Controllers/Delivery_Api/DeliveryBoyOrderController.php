e7259e22d7c78fbf450bb7328280fd03dc370787
Jinsi <jizjincy70@gmail.com>
2022-10-29 20:34:10 +0530

    delivered - store wallet notification

--- app/Http/Controllers/Delivery_Api/DeliveryBoyOrderController.php
+++ app/Http/Controllers/Delivery_Api/DeliveryBoyOrderController.php
@@ -805,6 +805,14 @@ class DeliveryBoyOrderController extends Controller
                             $clickAction = "MyWalletFragment";
                             $type = "wallet";
                             $data['response'] =  Helper::customerNotification($cd->customer_device_token, $title, $body,$clickAction,$type);
+
+                            $title = 'Store order points credited';
+                            $body = @$storeOrderPointAmount . ' points credited to your store wallet..';
+                            $clickAction = "MyWalletFragment";
+                            $type = "wallet";
+                            $data['response'] =  Helper::customerNotification($cd->customer_device_token, $title, $body,$clickAction,$type);
+
+
                         }
 
 
