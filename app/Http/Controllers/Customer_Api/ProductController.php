2824cb7edd190fd726c110fdb4c401344669bf9e
dipinpnr <dipin.kp@hexeam.co.in>
2022-10-30 09:29:25 +0530

    changes

--- app/Http/Controllers/Customer_Api/ProductController.php
+++ app/Http/Controllers/Customer_Api/ProductController.php
@@ -1476,7 +1476,7 @@ class ProductController extends Controller
         try {
             if (isset($request->customer_id) && Trn_store_customer::find($request->customer_id)) {
 
-                $totalCustomerRewardsCount = Trn_customer_reward::where('customer_id', $request->customer_id)->where('reward_point_status', 1)->whereNull('store_id')->sum('reward_points_earned');
+                $totalCustomerRewardsCount = Trn_customer_reward::where('customer_id', $request->customer_id)->where('reward_point_status', 1)->whereNull('store_id')->where('discription','!=','store_points')->sum('reward_points_earned');
                 $totalusedPoints = Trn_store_order::where('customer_id', $request->customer_id)->where('status_id', [5])->sum('reward_points_used');
                 $redeemedPoints = Trn_points_redeemed::where('customer_id', $request->customer_id)->sum('points');
 
@@ -1497,7 +1497,7 @@ class ProductController extends Controller
 
 
                 $data['customerRewards'] = Trn_customer_reward::where('customer_id',$request->customer_id)
-                    ->where('reward_point_status', 1)->whereNull('store_id')->orderBy('reward_id', 'DESC')->get();
+                    ->where('reward_point_status', 1)->whereNull('store_id')->where('discription','!=','store_points')->orderBy('reward_id', 'DESC')->get();
                 foreach ($data['customerRewards'] as $cr) {
                     if (Trn_customer_reward_transaction_type::find(@$cr->transaction_type_id)) {
                         $cr->rewardTransactionType = Trn_customer_reward_transaction_type::find(@$cr->transaction_type_id);
--- app/Http/Controllers/store/StoreController.php
+++ app/Http/Controllers/store/StoreController.php
@@ -2597,9 +2597,10 @@ class StoreController extends Controller
           }
         }
 
-       /* if (Trn_customer_reward::where('order_id', $order_id)->count() < 1) {
+       if (Trn_customer_reward::where('order_id', $order_id)->count() < 1) {
 
-          if ((Trn_customer_reward::where('order_id', $order_id)->count() < 1) || (Trn_store_order::where('customer_id', $customer_id)->count() == 1)) {*/
+          if ((Trn_customer_reward::where('order_id', $order_id)->count() < 1) || (Trn_store_order::where('customer_id', $customer_id)->count() == 1)) {
+            
             $cr = new Trn_customer_reward;
             $cr->transaction_type_id = 0;
             $cr->reward_points_earned = $orderPointAmount;
@@ -2608,7 +2609,7 @@ class StoreController extends Controller
             $cr->reward_approved_date = Carbon::now()->format('Y-m-d');
             $cr->reward_point_expire_date = Carbon::now()->format('Y-m-d');
             $cr->reward_point_status = 1;
-            $cr->discription = null;
+            $cr->discription = 'admin points';
             $cr->save();
 
             $scr = new Trn_customer_reward;
@@ -2641,8 +2642,8 @@ class StoreController extends Controller
               $type = "order";
               $data['response'] =  Helper::customerNotification($cd->customer_device_token, $title, $body,$clickAction,$type);
             }
-          //}
-       // }
+          }
+        }
       }
 
       if ($request->status_id == 8) {
