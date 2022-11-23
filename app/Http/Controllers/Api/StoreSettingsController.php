e26c8f4525372e6f02b457f9c543333da3b6b4d8
Jinsi <jizjincy70@gmail.com>
2022-11-23 14:37:29 +0530

    merchant create api to sandbox in cashfree

--- app/Http/Controllers/Api/StoreSettingsController.php
+++ app/Http/Controllers/Api/StoreSettingsController.php
@@ -416,7 +416,9 @@ class StoreSettingsController extends Controller
                     $curl = curl_init();
 
                     curl_setopt_array($curl, array(
-                        CURLOPT_URL => 'https://api.cashfree.com/api/v2/easy-split/vendors/' . $sBankDAta->vendor_id,
+                        // CURLOPT_URL => 'https://api.cashfree.com/api/v2/easy-split/vendors/' . $sBankDAta->vendor_id,
+                        CURLOPT_URL => 'https://test.cashfree.com/api/v2/easy-split/vendors/' . $sBankDAta->vendor_id,
+                        
                         CURLOPT_RETURNTRANSFER => true,
                         CURLOPT_ENCODING => '',
                         CURLOPT_MAXREDIRS => 10,
