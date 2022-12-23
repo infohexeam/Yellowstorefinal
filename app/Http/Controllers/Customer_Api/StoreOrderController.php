f3c5ede3f173dd78e2f7de0c1a975721d511fe0c
dipinpnr <dipin.kp@hexeam.co.in>
2022-12-23 11:49:02 +0530

    changes

--- app/Http/Controllers/Customer_Api/StoreOrderController.php
+++ app/Http/Controllers/Customer_Api/StoreOrderController.php
@@ -515,7 +515,7 @@ class StoreOrderController extends Controller
                     } else {
                         $orderNumberPrefix = 'ORDRYSTR';
                     }
-                    $last_order_number=Helper::checkOrderNumber(Auth::guard('store')->user()->store_id);
+                    $last_order_number=Helper::checkOrderNumber($request->store_id);
                     $orderNumber = $last_order_number + 1;
                     $order_no_exists=Trn_store_order::where('order_number',$orderNumberPrefix . @$orderNumber)->first();
                     if($order_no_exists)
@@ -927,7 +927,7 @@ class StoreOrderController extends Controller
                         } else {
                             $orderNumberPrefix = 'ORDRYSTR';
                         }
-                        $last_order_number=Helper::checkOrderNumber(Auth::guard('store')->user()->store_id);
+                        $last_order_number=Helper::checkOrderNumber($request->store_id);
                         $orderNumber = $last_order_number + 1;
                         
     
