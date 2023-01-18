ceedb23ad88edbc33a1505aad522b5ca219d04ab
dipinpnr <dipin.kp@hexeam.co.in>
2023-01-18 08:34:53 +0530

    order view image issue-2-customer order view

--- app/Http/Controllers/Customer_Api/StoreOrderController.php
+++ app/Http/Controllers/Customer_Api/StoreOrderController.php
@@ -2051,7 +2051,18 @@ class StoreOrderController extends Controller
 
                             $value['productDetail'] = Mst_store_product_varient::find($value->product_varient_id);
                             $vaproductDetail = Mst_store_product_varient::find($value->product_varient_id);
-                            @$value->productDetail->product_varient_base_image = '/assets/uploads/products/base_product/base_image/' . @$value->productDetail->product_varient_base_image;
+                            if(@$value->productDetail->product_varient_base_image!=NULL)
+                            {
+                             
+                             @$value->productDetail->product_varient_base_image = '/assets/uploads/products/base_product/base_image/' . @$value->productDetail->product_varient_base_image;
+ 
+                            }
+                            else
+                            {
+                             $baseProduct = Mst_store_product::find($value->product_id);
+                             @$value->productDetail->product_varient_base_image = '/assets/uploads/products/base_product/base_image/' .@$baseProduct->product_base_image;
+ 
+                            }
 
                             $baseProductDetail = Mst_store_product::find($value->product_id);
                             if (($baseProductDetail->product_type == 2) && ($baseProductDetail->service_type == 2)) {
