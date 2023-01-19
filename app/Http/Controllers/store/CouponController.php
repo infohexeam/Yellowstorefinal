6b95c1eb58d76d543739b7ec3874f23bc36a19b8
dipinpnr <dipin.kp@hexeam.co.in>
2023-01-19 13:02:26 +0530

    changs528

--- app/Http/Controllers/store/CouponController.php
+++ app/Http/Controllers/store/CouponController.php
@@ -1748,8 +1748,8 @@ class CouponController extends Controller
         $tot_prev_count[$i]=$tot_now_count[$i]-1;
         $cm=$orlink->commision_per_month;
         $co=$orlink->commision_per_order;
-        $d->previous_amount=$prev_amount[$i-1];
-        $d->new_amount=$prev_amount[$i-1]+@$co;
+        $d->previous_amount=$prev_amount[$i-1]+$cm;
+        $d->new_amount=$prev_amount[$i-1]+@$co+$cm;
         $prev_amount[$i]=$d->new_amount;
         $d->c_month= $cm;
         $d->c_order=$co;
