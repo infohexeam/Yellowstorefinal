afb94d9dcb37d68594d4d715b95e05d07a46ef7b
dipinpnr <dipin.kp@hexeam.co.in>
2023-01-19 15:49:20 +0530

    changs528

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -3074,7 +3074,7 @@ class StoreController extends Controller
                     $month_commision[$i]=$orlink->commision_per_month;
                     $sd->commission_month=$orlink->commision_per_month??$sd->delivery_boy_commision;
                     $sd->commission_order=$orlink->commision_per_order??$sd->delivery_boy_commision_amount;
-                    $sd->previous_commission=number_format($prev_amount[$i-1]+($sd->commission_month/$total_count));
+                    $sd->previous_commission=number_format($prev_amount[$i-1]);
                     $sd->commission_after_order=number_format($prev_amount[$i-1]+$sd->commission_order+($sd->commission_month/$total_count));
                     $prev_amount[$i]=$sd->commission_after_order;
                     //////////////////////////////////////////////
