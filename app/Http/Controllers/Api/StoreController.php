09ce6f0dc8901e08a7c6b9938864098d0797e4d0
dipinpnr <dipin.kp@hexeam.co.in>
2023-01-19 15:20:45 +0530

    changs528

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -3072,8 +3072,19 @@ class StoreController extends Controller
                     $sd->subadmin_phone=$sd->subadmindetail->phone??'';
                     $sd->commission_month=$orlink->commision_per_month??$sd->delivery_boy_commision;
                     $sd->commission_order=$orlink->commision_per_order??$sd->delivery_boy_commision_amount;
-                    $sd->previous_commission=$sd->commission_month+($tot_prev_count[$i]*@$sd->commission_order);
-                    $sd->commission_after_order=$sd->commission_month+($tot_now_count[$i]*@$sd->commission_order);
+                    $sd->previous_commission=$prev_amount[$i-1]+$sd->commission_month;
+                    $sd->commission_after_order=$prev_amount[$i-1]+$sd->commission_order+$sd->commission_month;
+                    $prev_amount[$i]=$sd->commission_after_order;
+                    //////////////////////////////////////////////
+        //             $cm=$orlink->commision_per_month;
+        // $co=$orlink->commision_per_order;
+        // $d->previous_amount=$prev_amount[$i-1];
+        // $d->new_amount=$prev_amount[$i-1]+@$co;
+        // $prev_amount[$i]=$d->new_amount;
+        // $d->c_month= $cm;
+        // $d->c_order=$co;
+
+
                     //////////////////////////////////////////////
                     
                    
