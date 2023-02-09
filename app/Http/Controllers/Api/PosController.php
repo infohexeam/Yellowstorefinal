f727e1295e6169fceae1fb8e991d4abd8152bb42
dipinpnr <dipin.kp@hexeam.co.in>
2023-02-09 11:53:10 +0530

    changes

--- app/Http/Controllers/Api/PosController.php
+++ app/Http/Controllers/Api/PosController.php
@@ -622,6 +622,10 @@ class PosController extends Controller
                         $product->tax_value = @$taxData->tax_value;
                
                     }
+                    $new = array_filter($data['productDetails'], function ($var) {
+                        return ($var['variant_status'] != 0);
+                    });
+                    $data['productDetails']=$new;
                     $data['status'] = 1;
                     $data['message'] = "success";
                     return response($data);
