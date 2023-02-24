8314d7a155b418c6674d41038b4516eb6802f395
dipinpnr <dipin.kp@hexeam.co.in>
2023-02-24 14:05:41 +0530

    changes

--- app/Http/Controllers/Customer_Api/ProductController.php
+++ app/Http/Controllers/Customer_Api/ProductController.php
@@ -1878,7 +1878,7 @@ class ProductController extends Controller
                 ->where('trn_wallet_logs.store_id',$request->store_id)
                 ->orderBy('trn_wallet_logs.wallet_log_id','DESC')
                 ->get();
-                $wallet_log_credited=Trn_wallet_log::where('customer_id',$request->customer_id)->whereNotNull('store_id')->sum('points_credited');
+                $wallet_log_credited=Trn_wallet_log::where('customer_id',$request->customer_id)->whereNotNull('store_id')->where('store_id',$request->store_id)->sum('points_credited');
                 $wallet_log_redeemed=Trn_wallet_log::where('customer_id',$request->customer_id)->whereNotNull('store_id')->whereNotNull('order_id')->sum('points_debited');
                 $available_points=$wallet_log_credited-$wallet_log_redeemed;              
                 $data['logs']=$wallet_logs;  
