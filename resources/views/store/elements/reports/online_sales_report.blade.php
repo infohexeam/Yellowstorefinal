08128293e7e03dedd9cdac980f45fedc8f0e23b2
Jinsi <jizjincy70@gmail.com>
2023-05-08 12:47:04 +0530

    683

--- resources/views/store/elements/reports/online_sales_report.blade.php
+++ resources/views/store/elements/reports/online_sales_report.blade.php
@@ -224,9 +224,6 @@
                 extend: 'pdf',
                 title: 'Online Sales Report',
                 footer: true,
-                exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-                 },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
             },
