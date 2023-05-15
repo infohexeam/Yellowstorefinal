760edfd9284e43b22a61461df8b900dd4b597fcf
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-15 16:10:40 +0530

    changes

--- app/Http/Controllers/Customer_Api/PurchaseController.php
+++ app/Http/Controllers/Customer_Api/PurchaseController.php
@@ -1030,7 +1030,7 @@ class PurchaseController extends Controller
                     $j=($h*$d)/100;
 
                     //$j=number_format((float)$j, 2, '.', '');
-                    if($j<=$relatableRedeemAmount)
+                    if($j<=$adminOrderAmount)
                     {
                         $adminOrderAmount=$relatableRedeemAmount;
                         $storeOrderAmount=$relatableRedeemAmount-$j;
@@ -1057,10 +1057,10 @@ class PurchaseController extends Controller
                     $g=$wallet_log_credited-$wallet_log_redeemed;//Trn_wallet_log::where('customer_id',$request->customer_id)->where('store_id',$store_id)->sum('points_credited');
                     ///$g=$request->store_wallet_balance;
                     $m=($g*$a)/100;
-                    return $relatableRedeemAmount;//153.76<=100
+                    //return $relatableRedeemAmount;//153.76<=100
 
                     //$m=number_format((float)$m, 2, '.', '');//Admin Redemption Points (Actual) (J)
-                    if($m<$relatableRedeemAmount)
+                    if($m<=$storeOrderAmount)
                     {
                         
                         $adminOrderAmount=$relatableRedeemAmount-$m;
