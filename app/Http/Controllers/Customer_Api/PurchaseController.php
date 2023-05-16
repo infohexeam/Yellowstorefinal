ef13d20a7f4d9d537e6837ac611bb0041f381208
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-16 10:58:33 +0530

    changes

--- app/Http/Controllers/Customer_Api/PurchaseController.php
+++ app/Http/Controllers/Customer_Api/PurchaseController.php
@@ -1013,8 +1013,7 @@ class PurchaseController extends Controller
        
             if (isset($request->order_amount) ) {
                 $orderAmount=$request->order_amount;
-            if($request->admin_points==1&&$request->store_points==1) 
-            { 
+           
                 if($redeem_preference==1)
                 {
                     $adminConfigPoints = Trn_configure_points::first();
@@ -1080,7 +1079,7 @@ class PurchaseController extends Controller
                     //return 2;
 
                 }
-            }
+        
                 if($request->admin_points==1)
                 {
                    
@@ -1095,7 +1094,7 @@ class PurchaseController extends Controller
                     //$h=$request->admin_wallet_balance;
                     //$h=30;
                     $j=($h*$d)/100;
-                    return $adminOrderAmount;
+                    //return $adminOrderAmount;
 
                     //$j=number_format((float)$j, 2, '.', '');//Admin Redemption Points (Actual) (J)
                     if($j<=$adminOrderAmount)
