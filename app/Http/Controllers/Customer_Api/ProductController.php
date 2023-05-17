8d4a49280e9bcc1adc77ed04e3904855647a9f31
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-17 13:53:03 +0530

    cjhanges

--- app/Http/Controllers/Customer_Api/ProductController.php
+++ app/Http/Controllers/Customer_Api/ProductController.php
@@ -129,7 +129,7 @@ class ProductController extends Controller
                 }
                 $data['productVartiantdata'] = $productVartiantdata;
                 $variant_stock_count=Mst_store_product_varient::where('product_id',$productData->product_id)->where('is_removed',0)->where('stock_count','>',0)->where('variant_status',1)->sum('stock_count');
-                if($variant_stock_count>0)
+                if($variant_stock_count<=0)
                 {
                     $data['message'] = 'No variants available';
                     $data['status'] = 0;
