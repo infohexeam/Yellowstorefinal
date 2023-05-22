c7c6f0597d264ec2563516d678e3f0603c655163
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-22 12:17:42 +0530

    cjhanges

--- app/Http/Controllers/Customer_Api/PurchaseController.php
+++ app/Http/Controllers/Customer_Api/PurchaseController.php
@@ -1726,7 +1726,7 @@ class PurchaseController extends Controller
                 //         //return response($data);
                 //     }
                 $rem=$data['orderAmount']-$data['totalReducableAmount'];
-                if($rem>0)
+                if($rem>1)
                 {
                     $data['remainingOrderAmount']=number_format((float)$rem, 2, '.', '');
 
