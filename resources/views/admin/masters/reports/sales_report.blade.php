58781ae1c5deccad080894b8121b7865a3a94b2e
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-25 15:55:32 +0530

    changes

--- resources/views/admin/masters/reports/sales_report.blade.php
+++ resources/views/admin/masters/reports/sales_report.blade.php
@@ -275,11 +275,7 @@
                 extend: 'pdf',
                 title: 'Sales report',
                 footer: true,
-                exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-                 },
-                 orientation : 'landscape',
-                pageSize : 'LEGAL',
+              
             },
             {
                 extend: 'excel',
