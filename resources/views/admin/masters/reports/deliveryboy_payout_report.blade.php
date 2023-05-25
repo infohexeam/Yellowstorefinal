0666e148d76a6280a63520916472f093cf7908a4
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-25 16:17:43 +0530

    changes

--- resources/views/admin/masters/reports/deliveryboy_payout_report.blade.php
+++ resources/views/admin/masters/reports/deliveryboy_payout_report.blade.php
@@ -215,9 +215,6 @@
                 extend: 'pdf',
                 title: 'Delivery Boy Payout Report',
                 footer: true,
-                exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-                 },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
             },
