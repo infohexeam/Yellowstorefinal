cd6417c0863ab2fa1448b6dff9c2d4d2e9589bff
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-25 16:29:51 +0530

    changes

--- resources/views/admin/masters/reports/redeem_report.blade.php
+++ resources/views/admin/masters/reports/redeem_report.blade.php
@@ -146,7 +146,7 @@
                                             <td>{{ $d->product_total_amount }}</td>
                                             <td>{{ $d->reward_points_used??'--' }}</td>
                                              <td>{{ $d->reward_points_used_store??'--' }}</td>
-                                            <td>{{ $d->amount_before_applying_rp }}</td>
+                                            
                                             <td>{{ $d->customer_first_name }} {{ $d->customer_last_name }}</td>
                                             <td>{{ $d->customer_mobile_number }}</td>
                                             
