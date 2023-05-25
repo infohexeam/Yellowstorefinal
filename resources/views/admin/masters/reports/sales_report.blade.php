df3efbf5c727560935db483c4116040f65b98269
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-25 15:53:36 +0530

    changes

--- resources/views/admin/masters/reports/sales_report.blade.php
+++ resources/views/admin/masters/reports/sales_report.blade.php
@@ -276,7 +276,7 @@
                 title: 'Sales report',
                 footer: true,
                 exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
                  },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
