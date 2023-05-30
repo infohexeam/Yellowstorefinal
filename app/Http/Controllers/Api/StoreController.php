7dd6c945fe5bac5542976c525522339058ffa07a
dipinpnr <dipin.kp@hexeam.co.in>
2023-05-30 14:32:19 +0530

    changes

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -3683,7 +3683,7 @@ class StoreController extends Controller
       {
         $item_price=Trn_store_order_item::where('order_id',$w->order_id)->sum('total_amount');
         //$w->amount_before_applying_rp=$item_price+$$w->amount_reduced_by_rp??0+$w->packing_charge??0+$w->delivery_charge??0+$w->amount_reduced_by_rp_store??0+$w->amount_reduced_by_coupon??0;
-        $w->amount_before_applying_rp=strval(number_format($item_price+$w->packing_charge+$w->delivery_charge,2));
+        $w->amount_before_applying_rp=strval(number_format($item_price+$w->packing_charge+$w->delivery_charge+$w->amount_reduced_by_coupon??0,2));
 
       }
       
--- resources/views/store/elements/reports/redeem_report.blade.php
+++ resources/views/store/elements/reports/redeem_report.blade.php
@@ -151,7 +151,7 @@
         //$d->amount_before_applying_rp=strval(number_format($item_price+$w->packing_charge+$w->delivery_charge,2));
 
                                             @endphp
-                                            <td>{{ number_format($item_price+$d->packing_charge+$d->delivery_charge,2) }}</td>
+                                            <td>{{ number_format($item_price+$d->packing_charge+$d->delivery_charge+$d->amount_reduced_by_coupon??0,2) }}</td>
                                            
                                             <td>{{ $d->customer_first_name }} {{ $d->customer_last_name }}</td>
                                             <td>{{ @$d->customer_mobile_number }}</td>
