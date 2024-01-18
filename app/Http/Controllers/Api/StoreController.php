c449a0ce636d41eb55fc54a2f14fbd4508944e06
dipinpnr <dipin.kp@hexeam.co.in>
2024-01-18 09:17:54 +0530

    link code update-4

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -129,14 +129,15 @@ class StoreController extends Controller
             $linkCode = ' ';
 
             if ($v->platform == 'Youtube') {
-                $revLink = strrev($v->video_code);
-                $revLinkCode = substr($revLink, 0, strpos($revLink, '='));
-                $linkCode = strrev($revLinkCode);
+                // $revLink = strrev($v->video_code);
+                // $revLinkCode = substr($revLink, 0, strpos($revLink, '='));
+                // $linkCode = strrev($revLinkCode);
 
-                if ($linkCode == "") {
-                    $revLinkCode = substr($revLink, 0, strpos($revLink, '/'));
-                    $linkCode = strrev($revLinkCode);
-                }
+                // if ($linkCode == "") {
+                //     $revLinkCode = substr($revLink, 0, strpos($revLink, '/'));
+                //     $linkCode = strrev($revLinkCode);
+                // }
+                $linkCode=Helper::getYouTubeVideoCode($v->video_code);
             }
 
             if (!isset($v->video_discription)) {
@@ -256,14 +257,15 @@ class StoreController extends Controller
             $linkCode = '';
 
             if ($v->platform == 'Youtube') {
-                $revLink = strrev($v->video_code);
-                $revLinkCode = substr($revLink, 0, strpos($revLink, '='));
-                $linkCode = strrev($revLinkCode);
+                // $revLink = strrev($v->video_code);
+                // $revLinkCode = substr($revLink, 0, strpos($revLink, '='));
+                // $linkCode = strrev($revLinkCode);
 
-                if ($linkCode == "") {
-                    $revLinkCode = substr($revLink, 0, strpos($revLink, '/'));
-                    $linkCode = strrev($revLinkCode);
-                }
+                // if ($linkCode == "") {
+                //     $revLinkCode = substr($revLink, 0, strpos($revLink, '/'));
+                //     $linkCode = strrev($revLinkCode);
+                // }
+                $linkCode=Helper::getYouTubeVideoCode($v->video_code);
             }
 
             if (!isset($v->video_discription)) {
