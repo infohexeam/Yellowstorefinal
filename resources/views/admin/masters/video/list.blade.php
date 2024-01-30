140757ad3c7dd97fd6a5a9b43b996f0de5b61f94
dipinpnr <dipin.kp@hexeam.co.in>
2024-01-30 11:59:35 +0530

    word wrap video description-2

--- resources/views/admin/masters/video/list.blade.php
+++ resources/views/admin/masters/video/list.blade.php
@@ -99,7 +99,7 @@ iframe{
                                        --
                                    @endif</td>
                                    <td>{{ @$value->town->town_name }} </td>
-                                   <td style="word-wrap: break-word;">{{ $value->video_discription }} </td>
+                                   <td style="white-space: normal;">{{ $value->video_discription }} </td>
                                       <td> 
                                        @if ($value->status == 1)
                                           Active
