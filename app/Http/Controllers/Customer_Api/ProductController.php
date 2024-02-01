2086193f4cca51b9ac28c569a8ad7e6882b45892
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-01 13:36:12 +0530

    push notification to store on product enquiry-2

--- app/Http/Controllers/Customer_Api/ProductController.php
+++ app/Http/Controllers/Customer_Api/ProductController.php
@@ -7780,7 +7780,7 @@ class ProductController extends Controller
             $store=Mst_store::find($store_id);
 
     
-            return response(['status' => 1, 'message' => 'Enquiry created','store_mobile'=>$store->store_mobile,'store_contact_person_number'=>$store->store_contact_person_phone_number,'responseEnquiryCustomer'=>$data['responseEnquiry'],'responseEnquiryStore'=>$data['responseEnquiryStore']]);
+            return response(['status' => 1, 'message' => 'Enquiry created','store_mobile'=>$store->store_mobile,'store_contact_person_number'=>$store->store_contact_person_phone_number,'responseEnquiryCustomer'=>$data['responseEnquiry']]);
         }
         catch (\Exception $e) {
             return response(['status' => 0, 'message' => $e->getMessage()]);
