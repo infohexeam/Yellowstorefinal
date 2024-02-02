3bde36ab403a04e31f4dbe5d4f38a7904b5a0d0c
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-02 13:07:59 +0530

    delivery address issue-first address default address-5

--- app/Http/Controllers/Customer_Api/CustomerController.php
+++ app/Http/Controllers/Customer_Api/CustomerController.php
@@ -1330,7 +1330,12 @@ class CustomerController extends Controller
                                 }
                                 else
                                 {
-                                    Trn_customerAddress::where('customer_address_id', $request->customer_address_id)->update(['default_status' => 0]);
+                                    if($request->customer_address_id!=$countAddress->customer_address_id)
+                                    {
+                                        Trn_customerAddress::where('customer_address_id', $request->customer_address_id)->update(['default_status' => 0]);
+
+                                    }
+                                    
                                 }
                                 
                             }
