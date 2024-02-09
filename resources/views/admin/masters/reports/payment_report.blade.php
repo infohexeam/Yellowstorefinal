411b529826781a19826d3820efe3d627727a67fe
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-09 08:17:46 +0530

    59-11

--- resources/views/admin/masters/reports/payment_report.blade.php
+++ resources/views/admin/masters/reports/payment_report.blade.php
@@ -219,6 +219,7 @@
     $(function(e) {
 	 $('#exampletable').DataTable( {
         dom: 'Bfrtip',
+         paging: false, // Disable pagination
         buttons: [
             {
                 extend: 'pdf',
