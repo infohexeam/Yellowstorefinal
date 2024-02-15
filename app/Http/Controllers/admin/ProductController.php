fea22e55044a5f409b686bd8971cc8e13ebf0ec2
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-15 09:19:02 +0530

    product visit report-716-7

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -1243,12 +1243,12 @@ class ProductController extends Controller
           'mst__sub_categories.sub_category_id',
           'mst__sub_categories.sub_category_name'
         )
-          ->leftjoin('trn_store_customers', 'trn_store_customers.customer_id', '=', 'trn__recently_visited_products.customer_id')
-          ->leftjoin('mst_stores', 'mst_stores.store_id', '=', 'trn__recently_visited_products.store_id')
-          ->leftjoin('mst_store_products', 'mst_store_products.product_id', '=', 'trn__recently_visited_products.product_id')
-          ->leftjoin('mst_store_product_varients', 'mst_store_product_varients.product_varient_id', '=', 'trn__recently_visited_products.product_varient_id')
-          ->leftjoin('mst_store_agencies', 'mst_store_agencies.agency_id', '=', 'mst_store_products.vendor_id')
-          ->leftjoin('mst_store_categories', 'mst_store_categories.category_id', '=', 'mst_store_products.product_cat_id')
+          ->join('trn_store_customers', 'trn_store_customers.customer_id', '=', 'trn__recently_visited_products.customer_id')
+          ->join('mst_stores', 'mst_stores.store_id', '=', 'trn__recently_visited_products.store_id')
+          ->join('mst_store_products', 'mst_store_products.product_id', '=', 'trn__recently_visited_products.product_id')
+          ->join('mst_store_product_varients', 'mst_store_product_varients.product_varient_id', '=', 'trn__recently_visited_products.product_varient_id')
+          ->join('mst_store_agencies', 'mst_store_agencies.agency_id', '=', 'mst_store_products.vendor_id')
+          ->join('mst_store_categories', 'mst_store_categories.category_id', '=', 'mst_store_products.product_cat_id')
           ->leftJoin('mst__sub_categories', 'mst__sub_categories.sub_category_id', '=', 'mst_store_products.sub_category_id')
           ->orderBy('trn__recently_visited_products.rvp_id', 'DESC')
           ->get();
