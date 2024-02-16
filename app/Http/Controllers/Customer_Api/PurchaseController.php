1f3ed213e6bc15ad5ad2a4b4a2a47b33d91a0512
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-16 12:43:25 +0530

    72-stock update service product-5

--- app/Http/Controllers/Customer_Api/PurchaseController.php
+++ app/Http/Controllers/Customer_Api/PurchaseController.php
@@ -3001,11 +3001,11 @@ public function addToCartTest(Request $request)
                            
                         
                         if (isset($varProdu)) { 
+                           
+                            
+                            if ($request->quantity <= $varProdu->stock_count) {  //quantity shud be less dan current stock
                             if($prData->product_type==1)
                             {
-
-                            if ($request->quantity <= $varProdu->stock_count) {  //quantity shud be less dan current stock
-               
                             $totcountInCart = Trn_Cart::where('customer_id', $request->customer_id)->where('remove_status','=',0)->count();
                             if ($totcountInCart == 0)
                             {
@@ -3084,13 +3084,13 @@ public function addToCartTest(Request $request)
                                 }
 
                             }
+                        }
 
                                 
                             } else {
                                 $data['message'] = 'Stock unavailablee';
                                 $data['status'] = 3;
                                 return response($data);
-                            }
                             }
                     
                     } else {
