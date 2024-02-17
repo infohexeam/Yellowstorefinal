74eecb8a4cbdf990fd2cfaa105deff578e1bb098
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-17 13:23:41 +0530

    77-8

--- app/Http/Controllers/Customer_Api/StoreOrderController.php
+++ app/Http/Controllers/Customer_Api/StoreOrderController.php
@@ -168,7 +168,7 @@ class StoreOrderController extends Controller
                                    
                             
                         }
-                        return $pua;
+                        //return $pua;
 
 
                     }
