0edb0b05d9cb0b63839c965a9249394ae9aa17e0
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-21 14:26:13 +0530

    516-8

--- resources/views/store/elements/password/update_password.blade.php
+++ resources/views/store/elements/password/update_password.blade.php
@@ -286,7 +286,7 @@ function validatePassChange() {
     } else {
         document.getElementById('showmessage').style.color = 'red';
         document.getElementById('showmessage').innerHTML = 'passwords not matching';
-         $('#submit').attr('disabled', fasle);
+         $('#submit').attr('disabled', false);
     }
    }
    else
