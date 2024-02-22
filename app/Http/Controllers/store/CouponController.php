0f9362db8af6a506529ec87c8ed8325a52c0834e
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-22 16:13:06 +0530

    603-1

--- app/Http/Controllers/store/CouponController.php
+++ app/Http/Controllers/store/CouponController.php
@@ -1730,6 +1730,9 @@ class CouponController extends Controller
         'trn_store_orders.created_at',
         'trn_store_orders.amount_reduced_by_coupon',
         'trn_store_orders.order_type',
+        'trn_store_orders.delivery_date',
+        'trn_store_orders.delivery_time',
+
 
         'trn_store_customers.customer_id',
         'trn_store_customers.customer_first_name',
