141ed9719cfaf0e0ff1471721dcba5d381a8cb83
dipinpnr <dipin.kp@hexeam.co.in>
2024-02-28 16:01:00 +0530

    838-3

--- resources/views/store/elements/coupon/create.blade.php
+++ resources/views/store/elements/coupon/create.blade.php
@@ -183,5 +183,12 @@ function disChange(dis)
    }
 }
 
+    function truncateInput(element, maxLength) {
+        if (element.value.length > maxLength) {
+            element.value = element.value.slice(0, maxLength);
+        }
+    }
+
+
 </script>
  @endsection
