3e6ad40e2c9c4db25ab201c81452571e3e48807b
dipinpnr <dipin.kp@hexeam.co.in>
2024-03-01 12:09:12 +0530

    683-new-27

--- resources/views/admin/masters/reports/online_sales_report.blade.php
+++ resources/views/admin/masters/reports/online_sales_report.blade.php
@@ -314,7 +314,7 @@ $(function(e) {
             },
             {
                 extend: 'excel',
-                title: ''Online Sales Report',
+                title: 'Online Sales Report',
                 footer: true,
                 exportOptions: {
                      columns:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
