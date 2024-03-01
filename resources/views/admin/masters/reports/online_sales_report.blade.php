9a39233c9293a5fcbfa25b6341988d72026a5128
dipinpnr <dipin.kp@hexeam.co.in>
2024-03-01 12:17:04 +0530

    changes

--- resources/views/admin/masters/reports/online_sales_report.blade.php
+++ resources/views/admin/masters/reports/online_sales_report.blade.php
@@ -307,7 +307,8 @@ $(document).ready(function() {
                 title: 'Online Sales Report',
                 footer: true,
                 exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+                     //columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+                     columns: ':visible' // Include all visible columns in export
                  },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
@@ -317,7 +318,8 @@ $(document).ready(function() {
                 title: 'Online Sales Report',
                 footer: true,
                 exportOptions: {
-                     columns:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+                     //columns:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+                     columns: ':visible' // Include all visible columns in export
                  }
             }
          ]
