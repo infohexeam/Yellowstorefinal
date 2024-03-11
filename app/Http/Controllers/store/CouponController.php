8d0afcdb2cb580ba1ffb7439fcc0810e5a437675
dipinpnr <dipin.kp@hexeam.co.in>
2024-03-11 11:12:49 +0530

    changes-new-603-2

--- app/Http/Controllers/store/CouponController.php
+++ app/Http/Controllers/store/CouponController.php
@@ -1533,6 +1533,8 @@ class CouponController extends Controller
         'trn_store_orders.order_type',
         'trn_store_orders.delivery_charge',
         'trn_store_orders.packing_charge',
+        'trn_store_orders.delivery_date',
+        'trn_store_orders.delivery_time',
 
         'trn_store_customers.customer_id',
         'trn_store_customers.customer_first_name',
