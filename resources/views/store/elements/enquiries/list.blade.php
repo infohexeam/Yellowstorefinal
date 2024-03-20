4e0c0642c66b50a95682408f5be5537387e9a8c8
dipinpnr <dipin.kp@hexeam.co.in>
2024-03-20 08:58:47 +0530

    push in enquiry list page-1

--- resources/views/store/elements/enquiries/list.blade.php
+++ resources/views/store/elements/enquiries/list.blade.php
@@ -134,7 +134,8 @@ $date = Carbon\Carbon::now();
 
 <!-- MESSAGE MODAL CLOSED -->
 
-
+<script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js"></script>
                       <script>
  $(document).ready(function() {
 $(function(e) {
@@ -163,7 +164,60 @@ $(function(e) {
 } );
 } );
 </script>
+<script>
+  var firebaseConfig = {
+     apiKey: "AIzaSyABJjLKVYHKL020Zdi8pbHsNS2ZLQ1Ka4Q",
+    authDomain: "yellowstore-web-application.firebaseapp.com",
+    projectId: "yellowstore-web-application",
+    storageBucket: "yellowstore-web-application.appspot.com",
+    messagingSenderId: "444886856017",
+    appId: "1:444886856017:web:935481722416346323e370",
+    measurementId: "G-VX5SKTNN3F"
+  };
+      
+    firebase.initializeApp(firebaseConfig);
+    const messaging = firebase.messaging();
+  
+  document.addEventListener("DOMContentLoaded", function(){
 
+            messaging
+            .requestPermission()
+            .then(function () {
+                console.log("working");
+                return messaging.getToken({ vapidKey: 'BP_JpvDrfy-521nigpqvOv5WTXBK5i5AMa2g8CvI-nXK_mxHAQFHHrtwJc-kLAWNBtM-AJjqEARZ_49e6Wph5e8' });
+            })
+            .then(function(token) {
+                console.log("token : "+token);
+     var _token = $('input[name="_token"]').val();
+     console.log("token_2 : "+_token);
+
+      $.ajax({
+            url:"{{ route('store.saveBrowserToken') }}",
+            method:"POST",
+            data:{token:token, _token:_token},
+            success:function(result)
+            {
+                console.log(result);
+            }
+       })         
+  
+            }).catch(function (err) {
+                console.log('User Chat Token Error'+ err);
+            });
+  });      
+    messaging.onMessage(function(payload) {
+        const noteTitle = payload.notification.title;
+        const noteOptions = {
+            body: payload.notification.body,
+            icon: payload.notification.icon,
+        };
+        // Play the alarm sound when a notification is received
+        const alarmSound = new Audio('https://hexprojects.in/Yellowstore/assets/alaram.wav');
+        alarmSound.play();
+        new Notification(noteTitle, noteOptions);
+    });
+   
+</script>
 
 
 
