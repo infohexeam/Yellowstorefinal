848d2e376feafc19ed1e277d92129ad7ea6ad292
dipinpnr <dipin.kp@hexeam.co.in>
2024-06-04 10:52:37 +0530

    changes on delivery boy payout report-7

--- resources/views/admin/masters/reports/deliveryboy_payout_report.blade.php
+++ resources/views/admin/masters/reports/deliveryboy_payout_report.blade.php
@@ -227,13 +227,14 @@
     $(function(e) {
 	 $('#exampletable').DataTable( {
         dom: 'Bfrtip',
+        autoWidth: false,
         buttons: [
             {
                 extend: 'pdf',
                 title: 'Delivery Boy Payout Report',
                 footer: true,
                 exportOptions: {
-                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,13,14,15,16]
+                     columns: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
                  },
                  orientation : 'landscape',
                 pageSize : 'LEGAL',
